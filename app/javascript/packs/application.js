f448b567c4a0a691ab41ded6a1f4f595c9e76458
Xavier Maisonnat <x.maisonnat@gmail.com>
2019-08-08 11:33:26 +0200

    setup the environment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,18 @@
 // const imagePath = (name) => images(name, true)
 
 console.log('Hello World from Webpacker')
+// config/webpack/environment.js
+const { environment } = require('@rails/webpacker')
+
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
+module.exports = environment
+import 'bootstrap';
